12c196042af41b9a12d5b10fef9e7697883c1b59
Zachary Petersen <zacharypetersen1@gmail.com>
2020-05-09 15:37:32 -0700

    Make search result component

---
+++ src/components/SearchResult.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import FormCheck from 'react-bootstrap/FormCheck'
+
+export default function SearchResult(props) {
+  const foodCache = useSelector((state) => state.search.foodCache);
+
+  return (
+    <div key={props.foodId} className="search-result">
+      <FormCheck inline className="search-result-check" />
+      <span>{foodCache[props.foodId].description}</span>
+      <span className="search-result-brand">{foodCache[props.foodId].brandOwner}</span>
+    </div>
+  )
+}
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,9 +2,9 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { handleLoadMore } from "../scripts/eventHandlers";
 import Button from "react-bootstrap/Button";
+import SearchResult from "./SearchResult";
 
 export default function SearchResults() {
-  const foodCache = useSelector((state) => state.search.foodCache);
   const lastQuery = useSelector((state) => state.search.lastQuery);
   const searchResults = useSelector((state) => state.search.searchResultCache[lastQuery]);
   const totalHits = useSelector((state) => state.search.totalHits[lastQuery]);
@@ -12,14 +12,9 @@ export default function SearchResults() {
   return (
     <div className="search-results-wrapper">
       <div className="search-results-container">
-        <ul>
-          {searchResults.map((foodId) => (
-            <li key={foodId}>
-              <span>{foodCache[foodId].description}</span>
-              <span style={{ marginLeft: "10px" }}>{foodCache[foodId].brandOwner}</span>
-            </li>
-          ))}
-        </ul>
+        {searchResults.map((foodId) => (
+          <SearchResult foodId={foodId} />
+        ))}
         {searchResults.length < totalHits ? <Button variant="outline-secondary" className="load-more-button" onClick={() => handleLoadMore()}>Load More Results</Button> : null}
       </div>
     </div>
