7fb48a9f19457498c3a52c7c1d92837d83ded60c
Zachary Petersen <zacharypetersen1@gmail.com>
2020-05-10 21:14:58 -0700

    Use flexboxes to format items in search list

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import FormCheck from 'react-bootstrap/FormCheck'
 
 export default function SearchResult(props) {
   const foodCache = useSelector((state) => state.search.foodCache);
 
   return (
     <div key={props.foodId} className="search-result">
-      {/*<FormCheck inline className="search-result-check" />*/}
-      <input type="checkbox" className="search-result-check" />
-      <span>{foodCache[props.foodId].description}</span>
-      <span className="search-result-brand">{foodCache[props.foodId].brandOwner}</span>
+      <div className="search-result-check-container">
+        <input type="checkbox" className="search-result-check" />
+      </div>
+      <div className="search-result-info-container">
+        <span>{foodCache[props.foodId].description}</span>
+        <span className="search-result-brand">{foodCache[props.foodId].brandOwner}</span>
+      </div>
     </div>
   )
 }
