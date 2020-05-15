dc81b45c9011fb40d05c0dd91335a43a9a1372be
Zachary Petersen <zacharypetersen1@gmail.com>
2020-05-15 16:39:51 -0700

    Set up structure for cart section

--- src/components/CartSection.js
+++ src/components/CartSection.js
@@ -3,7 +3,16 @@ import React from 'react'
 export default function CartSection() {
   return (
     <div className="cart-section">
-      
+      <div className="cart-header">
+        <div className="cart-name">
+          Name
+        </div>
+        <div className="cart-servings">
+          Servings
+        </div>
+      </div>
+      <div className="cart">
+      </div>
     </div>
   )
 }
