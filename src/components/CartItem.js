bf259914bc13b82ec8b4081f0e5bc843545db0d1
Zachary Petersen <zacharypetersen1@gmail.com>
2020-05-18 14:22:15 -0700

    Add x icon to cart items

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 export default function CartItem(props) {
   const foodCache = useSelector((state) => state.search.foodCache);
   
   return (
     <div className="cart-item">
-      <Button className="cart-item-remove" varient="danger">
-        X
-      </Button>
+      <div className="cart-item-remove">
+        <FontAwesomeIcon icon={ faTimes } />
+      </div>
       <div className="cart-item-text">
         {foodCache[props.id].description}
       </div>
