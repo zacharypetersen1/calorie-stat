7a70c21fe1e56c85cc29e20cce760469eb8a33b3
Zachary Petersen <zacharypetersen1@gmail.com>
2020-05-18 18:47:51 -0700

    Add servings section to cart items

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,7 +2,9 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removeFromCart } from "../actions/actions";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faTimes, faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
 export default function CartItem(props) {
   const foodCache = useSelector((state) => state.search.foodCache);
@@ -17,7 +19,13 @@ export default function CartItem(props) {
         {foodCache[props.id].description}
       </div>
       <div className="cart-item-servings">
-        serv
+        <Button className="servings-button">
+          <FontAwesomeIcon icon={ faMinus } />
+        </Button>
+        <FormControl className="servings-form" />
+        <Button className="servings-button">
+          <FontAwesomeIcon icon={ faPlus } />
+        </Button>
       </div>
     </div>
   )
