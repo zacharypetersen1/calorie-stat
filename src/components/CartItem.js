8e5d3758be59d573d0fc0df3f5169f4d503bfee4
Zachary Petersen <zacharypetersen1@gmail.com>
2020-05-18 17:43:13 -0700

    Add functionality to remove button within cart item

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import Button from "react-bootstrap/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { removeFromCart } from "../actions/actions";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 export default function CartItem(props) {
   const foodCache = useSelector((state) => state.search.foodCache);
+  const dispatch = useDispatch();
   
   return (
     <div className="cart-item">
-      <div className="cart-item-remove">
+      <div className="cart-item-remove" onClick={ () => dispatch(removeFromCart(props.id)) }>
         <FontAwesomeIcon icon={ faTimes } />
       </div>
       <div className="cart-item-text">
