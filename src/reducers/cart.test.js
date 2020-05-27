571ffae4290ab36fac26f924ebe40afe90a0662d
Zachary Petersen <zacharypetersen1@gmail.com>
2020-05-26 19:34:20 -0700

    Add testing for cart reducers

---
+++ src/reducers/cart.test.js
@@ -0,0 +1,40 @@
+import cart from "./cart.js";
+import * as types from "../actions/types";
+
+const initial = {
+  itemSet: new Set(),
+  itemList: [],
+  servingsMap: new Map(),
+};
+
+const afterAdd = {
+  itemSet: new Set([555]),
+  itemList: [555],
+  servingsMap: new Map([[555, 1]]),
+}
+
+describe("cart reducer", () => {
+  it("should return the initial state", () => {
+    expect(cart(undefined, {})).toEqual(initial);
+  });
+
+  it("should handle ADD_TO_CART", () => {
+    expect(
+      cart(initial, {type: types.ADD_TO_CART, payload: 555})
+    ).toEqual(afterAdd);
+  });
+
+  it("should handle REMOVE_FROM_CART", () => {
+    expect(
+      cart(afterAdd, {type: types.REMOVE_FROM_CART, payload: 555})
+    ).toEqual(initial);
+  });
+
+  it("should handle CHANGE_SERVINGS", () => {
+    expect(
+      cart(afterAdd, {type: types.CHANGE_SERVINGS, id: 555, payload: 7})
+    ).toEqual(
+      { ...afterAdd, servingsMap: new Map([[555, 7]]) }
+    );
+  });
+});
