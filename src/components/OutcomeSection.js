08ae984b380e52f240a11694b2e6d06d50f779cb
Zachary Petersen <zacharypetersen1@gmail.com>
2020-06-05 16:31:36 -0700

    Add graph container

--- src/components/OutcomeSection.js
+++ src/components/OutcomeSection.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function OutcomeSection() {
   return (
     <div className="outcome-section">
-      
+      <div className="graph-container">
+        
+      </div>
     </div>
   )
 }
