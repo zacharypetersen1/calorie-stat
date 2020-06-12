f983844b004d0efa14b74e066aa166bd0a557316
Zachary Petersen <zacharypetersen1@gmail.com>
2020-06-12 11:08:13 -0700

    Set up structure of Graph Elements

---
+++ src/components/GraphElement.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function GraphElement(props) {
+  return (
+    <div className="graph-element">
+      {props.name}
+    </div>
+  )
+}
--- src/components/OutcomeSection.js
+++ src/components/OutcomeSection.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import GraphElement from "./GraphElement";
 
 export default function OutcomeSection() {
+  const items = useSelector((state) => state.cart.items);
+  const foodCache = useSelector((state) => state.foods.cache);
+
   return (
     <div className="outcome-section">
       <div className="graph-container">
-        
+        {
+          [...items].map((id) => 
+            <GraphElement key={id} name={foodCache[id].description} />
+          )
+        }
       </div>
     </div>
   )
