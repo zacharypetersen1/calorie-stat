1a955f0a24b4ffd0c56e3a450c8c91e3cbffa06d
Zachary Petersen <zacharypetersen1@gmail.com>
2020-06-13 12:19:44 -0700

    Add color and size to graph elements

--- src/components/GraphElement.js
+++ src/components/GraphElement.js
@@ -2,7 +2,10 @@ import React from "react";
 
 export default function GraphElement(props) {
   return (
-    <div className="graph-element" style={{height: (props.percent*100).toString() + "%"}}>
+    <div 
+      className="graph-element" 
+      style={{"height": (props.percent*100).toString() + "%", "background-color": props.color}}
+    >
       {props.name}
     </div>
   )
--- src/components/OutcomeSection.js
+++ src/components/OutcomeSection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import GraphElement from "./GraphElement";
+import chroma from "chroma-js";
 
 export default function OutcomeSection() {
   const items = useSelector((state) => state.cart.items);
@@ -8,6 +9,9 @@ export default function OutcomeSection() {
   const nutrition = useSelector((state) => state.foods.nutrition);
   const servings = useSelector((state) => state.cart.servings);
 
+  const startColor = "#fafa6e";
+  const endColor = "#497d99";
+  const colors = items.size > 1 ? chroma.scale([startColor, endColor]).mode('lch').colors(items.size) : [startColor];
   const nutrientType = "sugar";
   const totalNutrient = [...items].reduce(
     (accumulator, id) => accumulator + parseFloat(nutrition.get(id)[nutrientType]) * servings.get(id), 0);
@@ -16,12 +20,13 @@ export default function OutcomeSection() {
     <div className="outcome-section">
       <div className="graph-container">
         {
-          [...items].map((id) => {
+          [...items].map((id, index) => {
             const nutrientAmount = parseFloat((nutrition.get(id)[nutrientType]) * servings.get(id));
             return <GraphElement 
               key={id} 
               name={foodCache[id].description}
               percent={nutrientAmount > 0 ? nutrientAmount / totalNutrient : 0}
+              color={colors[index]}
             />
           })
         }
