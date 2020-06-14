a628632f960da87d1330f32da77c4fdd3b0df914
Zachary Petersen <zacharypetersen1@gmail.com>
2020-06-14 12:53:52 -0700

    Add button section, remove outcome section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import SearchSection from "./components/SearchSection";
+import ButtonSection from "./components/ButtonSection";
 import CartSection from "./components/CartSection";
-import OutcomeSection from "./components/OutcomeSection";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./app.scss";
 
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="app-container">
       <SearchSection />
+      <ButtonSection />
       <CartSection />
-      <OutcomeSection />
     </div>
   );
 }
---
+++ src/components/ButtonSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ButtonSection() {
+  return (
+    <div className="button-section">
+      
+    </div>
+  )
+}
