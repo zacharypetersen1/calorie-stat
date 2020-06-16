b994eb2bc63bb0501ccc3d424582f08f14dc33e6
Zachary Petersen <zacharypetersen1@gmail.com>
2020-06-15 17:00:07 -0700

    Render graph within cart when not in edit mode

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -5,16 +5,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes, faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
+import classnames from "classnames";
 
 export default function CartItem(props) {
   const food = useSelector((state) => state.foods.cache[props.id]);
   const servings = useSelector((state) => state.cart.servings.get(props.id));
   const hasLoadedNutrition = useSelector((state) => state.foods.nutrition.get(props.id).isLoaded);
+  const displayMode = useSelector((state) => state.cart.mode);
   const hues = useSelector((state) => state.foods.hues);
   const dispatch = useDispatch();
   
   return (
-    <div className="cart-item" style={ {"background-color": `hsl(${hues.get(props.id)}, 50%, 60%`} }>
+    <div 
+      className={ classnames("cart-item", {"cart-item-fixed": displayMode !== "edit"}) }
+      style={ {
+        "backgroundColor": `hsl(${hues.get(props.id)}, 50%, 60%`,
+        "height": displayMode === "edit" ? "auto" : props.percent + "%",
+      } }
+    >
       <div className="cart-item-remove" onClick={ () => dispatch(removeFromCart(props.id)) }>
         <FontAwesomeIcon icon={ faTimes } />
       </div>
--- src/components/CartSection.js
+++ src/components/CartSection.js
@@ -6,6 +6,26 @@ import { useSelector } from "react-redux";
 
 export default function CartSection() {
   const items = useSelector((state) => state.cart.items);
+  const nutrition = useSelector((state) => state.foods.nutrition);
+  const servings = useSelector((state) => state.cart.servings);
+  const displayMode = useSelector((state) => state.cart.mode);
+  const isEdit = displayMode === "edit";
+  let cartItems;
+
+  if(isEdit) {
+    cartItems = [...items].map((id) => <CartItem key={id} id={id} percent={0} />);
+  } else {
+    const nutrientTotal = [...items].reduce(
+      (accumulator, id) => accumulator + parseFloat(nutrition.get(id)[displayMode]) * servings.get(id), 0);
+
+    cartItems = [...items].map((id) => 
+      <CartItem
+        key={id}
+        id={id}
+        percent={ (parseFloat(nutrition.get(id)[displayMode]) * servings.get(id) / nutrientTotal) * 100 }
+      />
+    );
+  }
 
   return (
     <div className="cart-section">
@@ -17,9 +37,12 @@ export default function CartSection() {
           Servings
         </div>
       </div>
-      <SimpleBar autoHide={false} className="section-scroll cart">
-        {[...items].map(id => <CartItem key={id} id={id} />)}
-      </SimpleBar>
+      {
+        isEdit ?
+        <SimpleBar autoHide={false} className="section-scroll cart"> {cartItems} </SimpleBar>
+        : <div className="section-scroll cart"> {cartItems} </div>
+      }
+
     </div>
   )
 }
